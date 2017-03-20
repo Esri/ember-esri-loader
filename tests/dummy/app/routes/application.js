c3af1c661bbf0dd347bc80e8b6cb62661e21de18
mjuniper <mjuniper@gmail.com>
2017-03-20 11:21:49 -0600

    use ember.run.bind instead of nested thens

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,15 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  esriLoader: Ember.inject.service('esri-loader'),
-
-  renderTemplate: function () {
-    // render the template as normal
-    this._super(...arguments);
-    // then preload the latest (4.x) version of the JSAPI
-    this.get('esriLoader').load().catch(err => {
-      // TODO: better way of showing error
-      window.alert(err.message || err);
-    });
-  }
+  // esriLoader: Ember.inject.service('esri-loader'),
+  //
+  // renderTemplate: function () {
+  //   // render the template as normal
+  //   this._super(...arguments);
+  //   // then preload the latest (4.x) version of the JSAPI
+  //   this.get('esriLoader').load().catch(err => {
+  //     // TODO: better way of showing error
+  //     window.alert(err.message || err);
+  //   });
+  // }
 });
