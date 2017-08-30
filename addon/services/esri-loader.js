cef29ad70d2609d6c6618be0989247688a907faf
Tom Wayson <tom@tomwayson.com>
2017-08-30 11:51:26 -0700

    DRY up the error handling code

--- addon/services/esri-loader.js
+++ addon/services/esri-loader.js
@@ -32,64 +32,56 @@ export default Ember.Service.extend({
     // otherwise create a promise that will resolve when the JSAPI is loaded
     this._loadPromise = new Ember.RSVP.Promise((resolve, reject) => {
       esriLoader.bootstrap(err => {
-        // need to kick of run loop in case we are in test mode
-        // see: https://discuss.emberjs.com/t/guide-asynchronous-side-effects-in-testing/2905
-        Ember.run(() => {
-          if (err) {
-            const message = err.message || err;
-            // has esriLoader.bootstrap already been called?
-            if (message === 'The ArcGIS API for JavaScript is already loaded.') {
-              // this can happen when there is more than one instance of this service
-              // running on the page at a time, for example, in acceptance tests
+        if (err) {
+          const message = err.message || err;
+          // has esriLoader.bootstrap already been called?
+          if (message === 'The ArcGIS API for JavaScript is already loaded.') {
+            // this can happen when there is more than one instance of this service
+            // running on the page at a time, for example, in acceptance tests
 
-              // TODO: this is _much_ better handled upstream
-              // so we want to get rid of all this once this issue is resolved:
-              // https://github.com/Esri/esri-loader/issues/28
+            // TODO: this is _much_ better handled upstream
+            // so we want to get rid of all this once this issue is resolved:
+            // https://github.com/Esri/esri-loader/issues/28
 
-              // first check if it's the same script
-              // NOTE: will haev to update this every time it's updated here:
-              // https://github.com/Esri/esri-loader/blob/master/src/esri-loader.ts#L29
-              const defaultUrl = 'https://js.arcgis.com/4.4/';
-              const url = options.url || defaultUrl;
-              const script = document.querySelector('script[data-esri-loader]');
-              if (script.src !== url) {
-                // user tried to load two different versions of the JSAPI
-                reject(err);
-              } else {
-                // check if the script has loaded yet
-                if (script.dataset.esriLoader === 'loaded' || esriLoader.isLoaded()) {
-                  // notify any watchers of isLoaded copmuted property
-                  this.notifyPropertyChange('isLoaded');
-                  // let the caller know that the API has been successfully loaded
-                  // TODO: would there be something more useful to return here?
-                  resolve({ success: true });
-                } else {
-                  // wait for the script to load and then resolve
-                  script.addEventListener('load', () => {
-                    // TODO: remove this event listener
-                    // more fun w/ Ember.run(), tests will fail w/o this
-                    Ember.run(() => {
-                      // notify any watchers of isLoaded copmuted property
-                      this.notifyPropertyChange('isLoaded');
-                      // let the caller know that the API has been successfully loaded
-                      // TODO: would there be something more useful to return here?
-                      resolve({ success: true });
-                    });
-                  }, false);
-                }
-              }
-            } else {
+            // first check if it's the same script
+            // NOTE: will haev to update this every time it's updated here:
+            // https://github.com/Esri/esri-loader/blob/master/src/esri-loader.ts#L29
+            const defaultUrl = 'https://js.arcgis.com/4.4/';
+            const url = options.url || defaultUrl;
+            const script = document.querySelector('script[data-esri-loader]');
+            if (script.src !== url) {
+              // user tried to load two different JSAPI scripts
               reject(err);
+            } else {
+              // check if the script has loaded yet
+              if (script.dataset.esriLoader === 'loaded' || esriLoader.isLoaded()) {
+                resolve();
+              } else {
+                // wait for the script to load and then resolve
+                script.addEventListener('load', () => {
+                  // TODO: remove this event listener
+                  resolve();
+                }, false);
+              }
             }
           } else {
-            // notify any watchers of isLoaded copmuted property
-            this.notifyPropertyChange('isLoaded');
-            // let the caller know that the API has been successfully loaded
-            // TODO: would there be something more useful to return here?
-            resolve({ success: true });
+            // not an error we can handle
+            reject(err);
           }
-        });
+        } else {
+          // no err
+          resolve();
+        }
       }, options);
+    })
+    .then(() => {
+      // update the isLoaded computed property
+      this.notifyPropertyChange('isLoaded');
+      // let the caller know that the API has been successfully loaded
+      // TODO: would there be something more useful to return here?
+      // bootstrap returns dojoRequire,
+      // but we want consumers to use loadModules instead
+      return { success: true };
     });
     return this._loadPromise;
   },
