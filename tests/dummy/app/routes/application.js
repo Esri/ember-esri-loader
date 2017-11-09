9a1f599d1d48386abe4482ddcf0fc2fd2c545566
Tom Wayson <tom@tomwayson.com>
2017-11-09 08:56:25 -0800

    loadModules() can now lazy load the ArcGIS API; default to v4.5
    bumped ersi-loader to 1.5.0 for the promise-based API and defaulting to the latest JSAPI (v.4.5)
    configure esri-loader to use Ember.RSVP.Promise

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -7,7 +7,8 @@ export default Ember.Route.extend({
     // render the template as normal
     this._super(...arguments);
     // then preload the latest (4.x) version of the JSAPI
-    this.get('esriLoader').load().catch(err => {
+    this.get('esriLoader').loadScript()
+    .catch(err => {
       // TODO: better way of showing error
       window.alert(err.message || err);
     });
